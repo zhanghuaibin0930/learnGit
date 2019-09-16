bd5b62441ccac11cddd1f6c8ef57de9447ed2e2b
zhanghb <zhanghb@autoai.com>
2019-09-16 11:34:21 +0800

    修改配置文件

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
+@SpringBootConfiguration
 @SpringBootApplication
 public class DemoApplication {
 
